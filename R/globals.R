373f5dfc0ec3cca84250b39aa38ab752b4e405e8
Svajunas Plunge <svajunas.plunge@gmail.com>
2021-02-05 19:50:30 +0200

    2021.02.05

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,2 @@
-utils::globalVariables(c("MON", "Day", "Month", "A", "M", "X4"))
+utils::globalVariables(c("MON", "Day", "Month", "A", "M", "X4", "CLIMATE",
+                         "MEASURE", "SETUP", "SUBBASIN", "Setup_name", "Subbasin", "basins"))
